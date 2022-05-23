7f4806f097cc90b321f6dba224c6795c3c7f5999
Harshit <harshit0579.be21@chitkara.edu.in>
2022-05-23 22:44:16 +0530

    a js file added

---
+++ Harshit.js1.js
@@ -0,0 +1,8 @@
+console.log("hi");
+
+Name: Code Runner
+Id: formulahendry.code-runner
+Description: Run C, C++, Java, JS, PHP, Python, Perl, Ruby, Go, Lua, Groovy, PowerShell, CMD, BASH, F#, C#, VBScript, TypeScript, CoffeeScript, Scala, Swift, Julia, Crystal, OCaml, R, AppleScript, Elixir, VB.NET, Clojure, Haxe, Obj-C, Rust, Racket, Scheme, AutoHotkey, AutoIt, Kotlin, Dart, Pascal, Haskell, Nim, 
+Version: 0.11.7
+Publisher: Jun Han
+VS Marketplace Link: https://marketplace.visualstudio.com/items?itemName=formulahendry.code-runner
